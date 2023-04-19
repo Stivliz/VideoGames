1d5e095b46089975c4e5f1f30a8be5ba4dabbcd1
stivliz <stivliz0909@gmail.com>
2023-04-18 23:35:03 -0500

    new changes

--- PI-Videogames/api/src/utils/getGames/bdGames.js
+++ PI-Videogames/api/src/utils/getGames/bdGames.js
@@ -13,17 +13,6 @@ const infoDb = async() => {
         }
     })
 
-        // const response = {
-        //     "id": game.id,
-        //     "name": game.name,
-        //     "background_image": game.background_image,
-        //     "description": game.description,
-        //     "genres": game.genres.map(genre => genre.name).join(' | '),
-        //     "platforms": game.platforms.map((e) => e.platform.name).join(' | '),
-        //     "released": game.released,
-        //     "rating": game.rating
-           
-        // }
     return game;
 }
 
